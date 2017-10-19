5d269b5c2ec76ecb4b033e991ab27a52e469d6a0
Darnell W <darnellwillis96@gmail.com>
2017-10-18 20:40:00 -0700

    friends

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -1,9 +1,32 @@
 var connections = [
     {
-      "name": "Ahmed",
-      "photo": "ahmed@example.com",
-      "scores": []
-    }
+      "name": "Carson",
+      "photo": "carson@gitkraken.com",
+      "scores": ["3", 
+                 "4", 
+                 "2", 
+                 "5", 
+                 "2", 
+                 "4", 
+                 "3", 
+                 "1", 
+                 "2", 
+                 "1"]
+    },
+    {
+        "name": "Alex",
+        "photo": "alex@apple.com",
+        "scores": ["3", 
+                   "5", 
+                   "2", 
+                   "1", 
+                   "4", 
+                   "3", 
+                   "5", 
+                   "1", 
+                   "4", 
+                   "2"]
+      }
   ];
   
   // Note how we export the array. This makes it accessible to other files using require.
